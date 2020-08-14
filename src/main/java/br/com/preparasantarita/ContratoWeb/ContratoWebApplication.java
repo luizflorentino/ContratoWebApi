5ded52fa9d134999081b8f1cd59282fa75d63abc
Luiz Augusto Bezerra Florentino Filho <luizflorentino@gmail.com>
2020-08-14 10:13:45 -0300

    Commit Inicial

---
+++ src/main/java/br/com/preparasantarita/ContratoWeb/ContratoWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.preparasantarita.ContratoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/preparasantarita/ContratoWeb/ContratoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.preparasantarita.ContratoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
